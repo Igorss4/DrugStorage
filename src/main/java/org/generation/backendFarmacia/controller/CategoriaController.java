b78d5a7a8aebd5e0bd21faf37bc629fbfd0aea8b
Igor Santos <igor.ox13@gmail.com>
2022-03-31 11:13:33 -0300

    Api farmacia

--- src/main/java/org/generation/backendFarmacia/controller/CategoriaController.java
+++ src/main/java/org/generation/backendFarmacia/controller/CategoriaController.java
@@ -52,7 +52,7 @@ public class CategoriaController {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
 	}
 	
-	@DeleteMapping
+	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
